a179f9ed547102f90013a73e7dbb70ad53ec1acb
MikeWitk <mike.witk@gmail.com>
2019-03-04 20:51:38 +0000

    'Hello.js', 'even.js', 'isOdd.js', 'ageCalculator.js', and 'howmanyhundreds.js' exercises completed

---
+++ ageCalculator.js
@@ -0,0 +1,5 @@
+function ageCalculator(name,yearOfBirth,currentYear){
+
+  var currentAge =  currentYear - yearOfBirth;
+  return name + " is " + currentAge + " years old.");
+}
---
+++ howmanyhundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(num) {
+
+   var divisionResult = num / 100;
+
+   var integer = Math.trunc(divisionResult);
+
+   return integer;
+
+
+}
