a508dea048c1aab6adf2dcceb2d0b47bbce8ec15
MikeWitk <mike.witk@gmail.com>
2019-03-05 18:02:40 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+var packinglist = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack");
+
+for (var i = 0; i< packinglist.length; i++){
+  console.log(packinglist[i]);
+}
